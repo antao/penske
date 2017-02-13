fbbdf98772895225aa3eb1dd6c876aa44c395481
João Antão <joao.antao@protonmail.com>
2017-02-13 23:03:27 +0100

    Add settings file

---
+++ settings.py
@@ -0,0 +1,17 @@
+# Your desktop folder
+folder=YOUR_FOLDER_PATH_TO_ORGANIZE
+
+# Debugging mode
+# If this option is True then extra debugging info will be printed
+debug = True
+
+# File extensions you need to organize
+# 'Folder' : [".extension"],
+extensions = {
+    'Images': [".jpg", ".gif", ".png", ".jpeg", ".bmp"],
+    'Audio': [".mp3", ".wav", ".aiff", ".flac", ".aac"],
+    'Video': [".m4v", ".flv", ".mpeg", ".mov", ".mpg", ".wmv", ".MOV", ".mp4"],
+    'Documents': [".doc", ".docx", ".txt", ".ppt", ".pptx", ".pdf", ".rtf"],
+    'Compressed':  [".zip", ".tar", ".7", ".rar"]
+}
+              
